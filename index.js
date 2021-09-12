374a75b7ddeda53df87dbfdbbcb928fabc313537
rasulkamolov <gitkamolov@gmail.com>
2021-09-12 21:03:11 +0500

    12sep 2021

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const greetToPerson = personName=>{
 } 
 
 console.log("Hello " + greetToPerson("Yoshi"));
+
+
+// block
+
+
+const getBirthYear = (age,firstname)=>{
+    const retirement = 2021-age;
+    return `${firstname} was born in ${retirement}`;
+}
+console.log(getRetairmentYrs(23,'Rasul'));
+
+// block 
+
+
+
+const cokeIngredients = (gram)=>{
+    return gram * 2;
+}
+
+const cokeMaking = (water,blackColor)=>{
+    const waterNeeded = cokeIngredients(water);
+    const colorNeeded = cokeIngredients(blackColor);
+    const preparedCoke = `${waterNeeded} gramm water and ${colorNeeded} gramm
+     black color are needed to make coke`;
+     return preparedCoke;
+}
+
+console.log(cokeMaking(20,15));
